b174fe007e0a1de7f7aec423944b286bebe1cbba
Pedro Lucas <p.lucass1501@gmail.com>
2026-02-27 11:29:39 -0300

    pegando dados

--- frontEnd/src/pages/Login/Login.jsx
+++ frontEnd/src/pages/Login/Login.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import styles from './Login.module.css'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [error, setError] = useState('')
+
+    const handlesubmit = async (e) =>{
+        e.preventDefault();
+
+        alert(`Usuário: ${email} \n Senha: ${senha}`)
+
+        //Chamar o context para verificar se o usuario existe e tem permissao, usando await!
+    }
+
     return(
     <>
         <section className={styles.container}>
-            <form className={styles.card}>
+            <form className={styles.card} onSubmit={handlesubmit}>
                 <h2 className={styles.title}>
                     Login
                 </h2>
@@ -13,13 +27,23 @@ const Login = () => {
                         <label htmlFor="username-input" className={styles.label}>
                             Usuário
                         </label>
-                        <input type="email" className={styles.inputLogin} required/>
+                        <input 
+                        type="email" 
+                        className={styles.inputLogin} 
+                        id='email-input'
+                        onChange={(e) => setEmail(e.target.value)}
+                        required/>
                     </div>
                     <div className={styles.inputGroup}>
                         <label htmlFor="password-input" className={styles.label}>
                             Senha
                         </label>
-                        <input type="text" className={styles.inputLogin} id='password-input' required/>
+                        <input 
+                        type="text" 
+                        className={styles.inputLogin} 
+                        id='password-input'
+                        onChange={(e) => setSenha(e.target.value)} 
+                        required/>
                     </div>
                     <button type='submit' className={styles.buttonForm}>
                         Entrar
