94be7d40544e99cad8493be9aa0662394657ff43
Pedro Lucas <p.lucass1501@gmail.com>
2026-03-19 11:30:04 -0300

    Alterando estrutura

---
+++ frontEnd/src/components/CardTurma/index.jsx
@@ -0,0 +1,11 @@
+import styles from './CardTurma.module.css'
+
+const CardTurma = () => {
+    return(
+    <div className={styles.cardContainer} >
+        <h5 className={styles.nameTurma}>TURMA 103</h5>
+    </div>
+    )
+}
+
+export default CardTurma
--- frontEnd/src/pages/Turmas/index.jsx
+++ frontEnd/src/pages/Turmas/index.jsx
@@ -1,9 +1,22 @@
-
-
+import CardTurma from '../../components/CardTurma'
+import styles from './Turmas.module.css'
 
 const Turmas = () => {
     return(
+        <>
         <h2>Pagina de Turmas</h2>
+        <section className={styles.containerCards}>
+            <CardTurma/>
+            <CardTurma/>
+            <CardTurma/>
+            <CardTurma/>
+            <CardTurma/>
+            <CardTurma/>
+            <CardTurma/>
+            <CardTurma/>
+            
+        </section>
+        </>
     )
 }
 
